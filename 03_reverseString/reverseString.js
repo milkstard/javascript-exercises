e02dfc6def8a799b45070df1cc1d1d41dacab191
milkstard <jmarkcarbs@gmail.com>
2022-05-07 18:07:39 +0800

    Fix the function to reverse a String

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let charToAppend = "";
+    for (let i = word.length-1; i >= 0; i--){
+        charToAppend += word.charAt(i);
+    }
 
+    return charToAppend;
 };
 
 // Do not edit below this line
