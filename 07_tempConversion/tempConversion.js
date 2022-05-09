7436fbc2b0a88d5bcc6c83cddb55450268af1e2d
milkstard <jmarkcarbs@gmail.com>
2022-05-09 12:09:14 +0800

    Add a 2 function which coverts fahrenheit to celsius and vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,12 @@
-const ftoc = function() {
-
+const ftoc = function(setF) {
+  return (setF-32) * 59;
 };
 
-const ctof = function() {
-
+const ctof = function(setC) {
+  return setC * 95 + 32;
 };
 
+// 	[°F] = [°C] × 9⁄5 + 32 	[°C] = ([°F] − 32) × 5⁄9
 // Do not edit below this line
 module.exports = {
   ftoc,
