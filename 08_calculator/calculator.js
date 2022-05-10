2bcdbc25bbe457c173450c6ac7d263f678ea0886
milkstard <jmarkcarbs@gmail.com>
2022-05-11 05:30:47 +0800

    Added function that are needed for calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(num1) {
+	return num1.reduce((prevNum, currentNum)=>{prevNum + currentNum},0);
 };
 
-const multiply = function() {
-
+const multiply = function(num1) {
+  return num1.reduce((prevNum, currentNum)=>{prevNum * currentNum},0);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1,num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+  let totalFact = num1;
+  if(num1 === 0 || num1 === 1){
+    return 1;
+  }else{
+    num1--; //since we are starting from num1 itself we will be subtracting 1 to it.
+    while(num1 > 1){
+      totalFact *= num1;
+    }
+  }
+  return totalFact;
 };
 
 // Do not edit below this line
