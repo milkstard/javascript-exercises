c1d31ab4ade81ea6e51413016b5badbcf4e8a33b
milkstard <jmarkcarbs@gmail.com>
2022-05-11 16:39:02 +0800

    Add a fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num1) {
+    let previousFibo = 0;
+    let currentFibo = 1;
+    let nextFibo = 0;
+        
+    if(!Number.isInteger(num1)){
+        num1 = parseInt(num1);
+    }
 
+    for( let i = 1; i <= num1; i++){
+        nextFibo = previousFibo + currentFibo;
+        previousFibo = currentFibo;
+        currentFibo = nextFibo;
+    }
+    
+    return previousFibo;
 };
 
 // Do not edit below this line
