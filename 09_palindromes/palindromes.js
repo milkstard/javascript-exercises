2b1e40a6a378df126ba5aa6273bc170226a5e1ac
milkstard <jmarkcarbs@gmail.com>
2022-05-11 12:31:04 +0800

    Add fuction to check if palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,11 @@
-const palindromes = function () {
+const palindromes = function (x) {
+    //remove whitespace
+    let wsRemove = x.replace(/\s/g, '');
+    //remove non-alphanumeric
+    let naRemove = wsRemove.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+    
+    //let finalResult = naRemove.split('').reverse().join("").localeCompare(naRemove);
+    return (!naRemove.split('').reverse().join("").localeCompare(naRemove));
 
 };
 
