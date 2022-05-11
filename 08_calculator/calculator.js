75c34b8436915405dff4b646b2974e3100f8ddac
milkstard <jmarkcarbs@gmail.com>
2022-05-11 11:33:42 +0800

    Delete .skip

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -7,11 +7,11 @@ const subtract = function(num1, num2) {
 };
 
 const sum = function(num1) {
-	return num1.reduce((prevNum, currentNum)=>{prevNum + currentNum},0);
+	return num1.reduce((prevNum, currentNum) => {return prevNum + currentNum}, 0);
 };
 
 const multiply = function(num1) {
-  return num1.reduce((prevNum, currentNum)=>{prevNum * currentNum},0);
+  return num1.reduce((prevNum, currentNum) => {return prevNum * currentNum}, 1);
 };
 
 const power = function(num1, num2) {
@@ -23,9 +23,10 @@ const factorial = function(num1) {
   if(num1 === 0 || num1 === 1){
     return 1;
   }else{
-    num1--; //since we are starting from num1 itself we will be subtracting 1 to it.
+    num1 = num1-1; //since we are starting from num1 itself we will be subtracting 1 to it.
     while(num1 > 1){
       totalFact *= num1;
+      num1--;
     }
   }
   return totalFact;
